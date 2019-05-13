5406e838abdc8aaa1a098c8c39fa55a5e88d8864
oref <ferovax@gmail.com>
2019-05-13 22:18:27 +0100

    added plans fo rthis

--- bujo.py
+++ bujo.py
@@ -2,6 +2,11 @@ import click
 import getpass
 from pyfiglet import Figlet
 
+#bujo new todo -t board
+# new todo list -t = type "board"
+#bujo todo
+#bujo todo tomorrow -l
+
 
 @click.group()
 def cli():
@@ -9,19 +14,32 @@ def cli():
 
 
 @cli.command()
-@click.option('--custom', default=False)
+@click.option('--custom', '-c', default=False)
 def fig(custom):
     """Prints PyBujo or a cool message"""
     f = Figlet(font='slant')
     if custom:
         print(f.renderText(custom))
     else:
-        print(f.renderText('PyBujo'))
+        print(f.renderText(getpass.getuser()))
 
 @cli.command()
 @click.option("-n" or "--name", default=False)
-def config(name=getpass.getuser()):
+def config():
+    """
+    Either opens config file for editing or provides
+        options
+    """
     pass
+
+def new():
+    """
+    Creates a new journal
+    """
+    pass
+
+
+
 
 
 if __name__ == '__main__':
