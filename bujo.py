a9c45db70d75638db26b5e957bb738b4b64d1dfe
Harry Morgan <harry.morgan@dai.co.uk>
2019-05-14 16:36:07 +0100

    added yaml r w and adding notes

--- bujo.py
+++ bujo.py
@@ -1,11 +1,16 @@
 import click
 import getpass
 from pyfiglet import Figlet
+import os.path
+import yaml
 
 #bujo new todo -t board
 # new todo list -t = type "board"
 #bujo todo
 #bujo todo tomorrow -l
+
+
+_BUJO_PATH = os.path.join(os.path.expanduser('~'), 'bujo.yaml')
 
 
 @click.group()
@@ -23,23 +28,45 @@ def fig(custom):
     else:
         print(f.renderText(getpass.getuser()))
 
+
 @cli.command()
-@click.option("-n" or "--name", default=False)
-def config():
+@click.argument('note', type=str)
+@click.option('-b', '--bujo')
+def add(note, bujo=None):
     """
-    Either opens config file for editing or provides
-        options
+    Adds a note to the corresponding bujo
+    :param note: The note to add
+    :param bujo: The journal to add it to
+    :return:
     """
-    pass
+    data = _yaml_r() or {}
+    if bujo is None:
+        bujo = 'General'
 
-def new():
-    """
-    Creates a new journal
-    """
-    pass
+    try:
+        if note not in data[bujo]:
+            data[bujo].append(note)
+        else:
+            click.echo("You've already made this note")
+    except KeyError:
+        data[bujo] = [note]
+
+    _yaml_w(data)
 
 
+def _yaml_r():
+    try:
+        with open(_BUJO_PATH, 'r') as bujo_file:
+            return yaml.safe_load(bujo_file)
+    except FileNotFoundError:
+        with open(_BUJO_PATH, 'w+'):
+            ...
+        _yaml_r()
 
+
+def _yaml_w(data):
+    with open(_BUJO_PATH, 'w') as bujo_file:
+        yaml.dump(data, bujo_file, indent=4, default_flow_style=False)
 
 
 if __name__ == '__main__':
